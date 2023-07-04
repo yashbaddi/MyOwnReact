62f04a2f13adbeee90816d75ce8aae5b3c5ef657
Yash Baddi <yashbaddi29@gmail.com>
2023-07-04 11:10:33 +0530

    made changes on element props

--- render.js
+++ render.js
@@ -1,7 +1,9 @@
 import { createDOMElement } from "./createDOMElement.js";
 
 export function render(element, container) {
-  const DOMElement = createDOMElement(element.type);
+  const children = element.props.children;
+  delete element.props.children;
+  const DOMElement = createDOMElement(element.type, element.props);
   element.props.children.forEach((child) => {
     render(child, DOMElement);
   });
