ed336a45ae6ef86652c5a7cd1be33b4082ff8479
Yash Baddi <yashbaddi29@gmail.com>
2023-07-05 23:24:34 +0530

    Modified createElement

--- createDOM.js
+++ createDOM.js
@@ -1,9 +1,22 @@
 import { createDOMElement, createTextNode } from "./DOMManupulation.js";
 
 export function createDOM(fiber) {
+  const filteredProps = propsWithoutChildren(fiber.props);
   const DOMElement =
     fiber.type === "TEXT_ELEMENT"
       ? createTextNode(element.props.nodeValue)
-      : createDOMElement(element.type, element.props);
+      : createDOMElement(element.type, filteredProps);
   return DOMElement;
+}
+
+function propsWithoutChildren(props) {
+  const filtered = Object.keys(props)
+    .filter((key) => key != "children")
+    .reduce((obj, key) => {
+      return Object.assign(obj, {
+        [key]: user[key],
+      });
+    }, {});
+
+  return filtered;
 }
