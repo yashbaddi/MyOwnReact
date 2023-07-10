2159ff432fadee6bedf43fffd46bc75181c9cbc8
Yash Baddi <yashbaddi29@gmail.com>
2023-07-10 09:37:01 +0530

    ✨ADDED: changeNextUnitOfWork

--- Utils/workLoopGen.js
+++ Utils/workLoopGen.js
@@ -1,13 +1,15 @@
+let nextUnitOfWork = null;
 export default function workLoopGen(
   performUnitOfWork,
   firstUnitofWork,
   postDeadline = () => {}
 ) {
-  let nextUnitOfWork = firstUnitofWork;
+  nextUnitOfWork = firstUnitofWork;
 
   function workLoop(deadline) {
     let shouldYield = false;
     while (nextUnitOfWork && !shouldYield) {
+      console.log("next unit of work", nextUnitOfWork);
       nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
       shouldYield = deadline.timeRemaining() < 1;
     }
@@ -18,4 +20,8 @@ export default function workLoopGen(
   }
 
   requestIdleCallback(workLoop);
+}
+
+export function changeNextUnitOfWork(fiber) {
+  nextUnitOfWork = fiber;
 }
